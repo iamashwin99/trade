2b24dfa9e96a2637d15a8c11180f1cbbd0724886
Rahulghuge94 <1994ghuge@gmail.com>
2020-12-02 00:41:09 +0530

    Update _kiteconnect.py

--- trade/_kiteconnect.py
+++ trade/_kiteconnect.py
@@ -2,15 +2,10 @@ import time
 import json
 import datetime as dt
 import requests
+      
+s=requests.session()
 
-class kite:
-    def __init__(self):       
-        self.session=requests.session()
-#ent=0
-#    for token in mkt['data']:
-#	tokens.update[{t['data'][ent]['tradingsymbol']:t['data'][ent]['instrument_token']}]
-#	ent=ent+1
-        symbols=['IBULHSGFIN','AUROPHARMA','ADANIGREEN','CUMMINSIND','INDHOTEL','CHOLAFIN','FORTIS','COROMANDEL','M&MFIN','CADILAHC','NAUKRI','AARTIIND','SHREECEM','TORNTPHARM','ADANITRANS','BALKRISIND','GRASIM','ADANIGAS','HINDZINC',
+symbols=['IBULHSGFIN','AUROPHARMA','ADANIGREEN','CUMMINSIND','INDHOTEL','CHOLAFIN','FORTIS','COROMANDEL','M&MFIN','CADILAHC','NAUKRI','AARTIIND','SHREECEM','TORNTPHARM','ADANITRANS','BALKRISIND','GRASIM','ADANIGAS','HINDZINC',
          'PAGEIND','BATAINDIA','SRF','SUNPHARMA','BIOCON','ABCAPITAL','ASHOKLEY','JUBLFOOD','SIEMENS','ITC','BAJAJHLDNG','INFRATEL','COFORGE','EDELWEISS','ADANIENT','BHEL','APOLLOTYRE','MANAPPURAM','EMAMILTD','GLENMARK','L&TFH','OIL',
          'RAMCOCEM','NAM-INDIA','VGUARD','PETRONET','GSPL','LTTS','TATACHEM','EICHERMOT','HDFCLIFE','IPCALAB','DALBHARAT','HINDUNILVR','JSWENERGY','MGL','ZEEL','DABUR','HINDALCO','SAIL','CIPLA','TATAMOTORS','GICRE','ESCORTS','NMDC','MRF',
          'UBL','PEL','NAVINFLUOR','TECHM','SYNGENE','CROMPTON','DIVISLAB','LUPIN','MOTHERSUMI','GAIL','BAJAJFINSV','IDEA','HEROMOTOCO','TITAN','LT','EXIDEIND','PRESTIGE','APLLTD','INDIGO','TATACONSUM','MINDTREE','SANOFI','HUDCO','ENDURANCE',
@@ -19,58 +14,61 @@ class kite:
          'SBILIFE','BHARATFORG','MFSL','BPCL','TORNTPOWER','ABFRL','DRREDDY','POWERGRID','MARUTI','PIIND','INFY','NATIONALUM','HDFCBANK','ASIANPAINT','ADANIPORTS','DMART','ICICIGI','GODREJCP','PFC','TATASTEEL','GUJGASLTD','BANKINDIA','TCS',
          'DLF','ABBOTINDIA','FEDERALBNK','AJANTPHARM','JINDALSTEL','CESC','MARICO','HDFC','POLYCAB','NTPC','UPL','PFIZER','CUB','VOLTAS','JSWSTEEL','AXISBANK','RECLTD','ONGC','PGHH','TATAPOWER','WHIRLPOOL','VBL','MCDOWELL-N','IGL','INDUSINDBK',
          'SBIN','ICICIBANK','COALINDIA','YESBANK','SBICARD','CANBK','BANKBARODA','BOSCHLTD','KOTAKBANK','LICHSGFIN','APOLLOHOSP','SRTRANSFIN']
+res=""
+resp1=""
+referer="https://kite.zerodha.com/static/build/chart.html?v=2.6.1"
+method="get"
+path=""
+enctoken=""
 
-        referer="https://kite.zerodha.com/static/build/chart.html?v=2.6.1"
-        method="get"
-        path=""
-
-def login():
-    if driver.current_url!="https://kite.zerodha.com/":
-       driver.get("https://kite.zerodha.com/")
-    
-    driver.find_element(By.ID, "userid").click()
-    driver.find_element(By.ID, "userid").send_keys("YF6709")
-    driver.find_element(By.ID, "password").send_keys("Rahul@123")
-    driver.find_element(By.ID, "password").click()
-    driver.find_element(By.CSS_SELECTOR, ".button-orange").click()
-    time.sleep(3)
-    driver.find_element(By.ID, "pin").send_keys("159753")
-    driver.find_element(By.ID, "pin").send_keys(Keys.ENTER)
-    driver.find_element(By.CSS_SELECTOR, ".button-orange").click()
-
-def orderbook(S=S):
+def login(useid, password,twofa):
+    s.get("https://kite.zerodha.com/")
+    rawcookie=self.session.cookies.get_dict()
+    try:
+        cookie="__cfduid="+rawcookie["__cfduid"]+"; kf_session="+rawcookie["kf_session"]
+    except:
+        print("not received cookies slow network. please retry.")
+    h={"Host": "kite.zerodha.com","cache-control": "max-age=0","save-data": "on","upgrade-insecure-requests": "1","user-agent": "Mozilla/5.0 (Linux; Android 7.1.2; Redmi 5A) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Mobile Safari/537.36","sec-fetch-mode":"navigate","sec-fetch-user": "?1","dnt": "1",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3","sec-fetch-site": "none","accept-encoding": "gzip, deflate, br","accept-language": "en-US,en;q=0.9,mr;q=0.8,hi;q=0.7","x-csrftoken":"jDFGuxgIODRRA26hsMYXr203zdckPO8X",
+        "cookie":cookie}
+    res=s.post("https://kite.zerodha.com/api/login",headers=h,data={"user_id":useid,"password": password})
+    h1={"authority": "kite.zerodha.com","method": "POST","path": "/api/twofa",
+    "scheme": "https","accept": "application/json, text/plain, */*","accept-encoding": "gzip, deflate, br","accept-language": "en-US,en;q=0.9,mr;q=0.8,hi;q=0.7","cache-control": "no-cache","content-length": "109","content-type": "application/x-www-form-urlencoded","cookie":cookie,"dnt": "1","origin": "https://kite.zerodha.com","pragma": "no-cache",
+    "referer": "https://kite.zerodha.com/","sec-fetch-dest": "empty","sec-fetch-mode": "cors","sec-fetch-site": "same-origin","user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36","x-csrftoken": "jDFGuxgIODRRA26hsMYXr203zdckPO8X","x-kite-userid": "YF6709","x-kite-version": "2.6.3"}
+    resp1=s.post("https://kite.zerodha.com/api/twofa",headers=h1,data={"user_id":"YF6709","request_id":b.json()["data"]["request_id"],"twofa_value":twofa})
+    enctoken=s.cookies.get_dict()['enctoken']ef orderbook():
     referer="https://kite.zerodha.com/dashboard"
     method="get"
     path="/oms/orders"
-    return S.get("https://kite.zerodha.com/oms/orders",headers=head).json()
+    
 
-def marketwatch(S=S):
+def marketwatch():
     referer="https://kite.zerodha.com/dashboard"
     method="get"
     path="/api/marketwatch"
     headers1={"authority": "kite.zerodha.com","method": method,"path": path,"scheme": "https","Accept":"*/*","Accept-Encoding":"utf-8","Accept-Language":"en-US,en;q=0.5","authorization":enctoken,"Connection":"keep-alive",
               "Cookie":cookie,"Host":"kite.zerodha.com","Referer":referer,"sec-fetch-dest":"empty","sec-fetch-mode": "cors","sec-fetch-site": "same-origin",
               "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0","x-csrftoken":pubtoken,'x-kite-version': '2.6.2'}
-    mkt=S.get("https://kite.zerodha.com/api/marketwatch",headers=headers1).json()
+    mkt=s.get("https://kite.zerodha.com/api/marketwatch",headers=headers1).json()
     return mkt
 
-        def position(S=S):
-            referer="https://kite.zerodha.com/dashboard"
-            method="get"
-            path="/oms/portfolio/positions"
-            headers1={"authority": "kite.zerodha.com","method": method,"path": path,"scheme": "https","Accept":"*/*","Accept-Encoding":"utf-8","Accept-Language":"en-US,en;q=0.5","authorization":enctoken,"Connection":"keep-alive",
-              "Cookie":cookie,"Host":"kite.zerodha.com","Referer":referer,"sec-fetch-dest":"empty","sec-fetch-mode": "cors","sec-fetch-site": "same-origin",
-              "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0",'x-kite-version': '2.6.2'}
-            return S.get("https://kite.zerodha.com/oms/portfolio/positions",headers=headers1).json
+def position(S=S):
+    referer="https://kite.zerodha.com/dashboard"
+    method="get"
+    path="/oms/portfolio/positions"
+    headers1={"authority": "kite.zerodha.com","method": method,"path": path,"scheme": "https","Accept":"*/*","Accept-Encoding":"utf-8","Accept-Language":"en-US,en;q=0.5","authorization":enctoken,"Connection":"keep-alive",
+    "Cookie":cookie,"Host":"kite.zerodha.com","Referer":referer,"sec-fetch-dest":"empty","sec-fetch-mode": "cors","sec-fetch-site": "same-origin",
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0",'x-kite-version': '2.6.2'}
+     return s.get("https://kite.zerodha.com/oms/portfolio/positions",headers=headers1).json
 
-        def fund(S=S):
-            referer="https://kite.zerodha.com/dashboard"
-            method="get"
-            path="/oms/user/margins"
-            headers1={"authority": "kite.zerodha.com","method": method,"path": path,"scheme": "https","Accept":"*/*","Accept-Encoding":"utf-8","Accept-Language":"en-US,en;q=0.5","authorization":enctoken,"Connection":"keep-alive",
-                      "Cookie":cookie,"Host":"kite.zerodha.com","Referer":referer,"sec-fetch-dest":"empty","sec-fetch-mode": "cors","sec-fetch-site": "same-origin",
-                      "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0",'x-kite-version': '2.6.2'}
-            return S.get("https://kite.zerodha.com/oms/user/margins",headers=headers1).json()
+def fund():
+    referer="https://kite.zerodha.com/dashboard"
+    method="get"
+    path="/oms/user/margins"
+    headers1={"authority": "kite.zerodha.com","method": method,"path": path,"scheme": "https","Accept":"*/*","Accept-Encoding":"utf-8","Accept-Language":"en-US,en;q=0.5","authorization":enctoken,"Connection":"keep-alive",
+    "Cookie":cookie,"Host":"kite.zerodha.com","Referer":referer,"sec-fetch-dest":"empty","sec-fetch-mode": "cors","sec-fetch-site": "same-origin",
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0",'x-kite-version': '2.6.2'}
+     return S.get("https://kite.zerodha.com/oms/user/margins",headers=headers1).json()
 
 def plceorder(price,quantity,symbol):
     referer="https://kite.zerodha.com/dashboard"
@@ -102,13 +100,6 @@ def getdata(token,frdate,todate):
     return S.get(url,headers=headers1).json()    
 login()
 time.sleep(3)
-try:    
-    cookie="__cfduid="+driver.get_cookies()[4]['value']+"; kf_session="+driver.get_cookies()[3]['value']+"; public_token="+driver.get_cookies()[2]['value']+"; user_id=YF6709"+"; enctoken="+driver.get_cookies()[1]['value']
-except:
-    print("error")
-
-enctoken="enctoken "+driver.get_cookies()[1]['value']
-pubtoken=driver.get_cookies()[2]['value']
 
 head={"authority": "kite.zerodha.com","method": method,"path": path,"scheme": "https","Accept":"*/*","Accept-Encoding":"utf-8","Accept-Language":"en-US,en;q=0.5","authorization":enctoken,
       "Connection":"keep-alive","Cookie":cookie,"Host":"kite.zerodha.com","Referer":referer,"sec-fetch-dest": "empty","sec-fetch-mode": "cors","sec-fetch-site": "same-origin",
