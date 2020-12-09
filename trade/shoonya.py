dc664fa4e8207b5d7a9995e3c6e25701b87c4695
Rahulghuge94 <1994ghuge@gmail.com>
2020-12-09 20:45:46 +0530

    Update shoonya.py

--- trade/shoonya.py
+++ trade/shoonya.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 s=requests.session()
 response=""
-def login(response,email, password,pan):
+def login(email, password,pan):
     headers={'Accept':'application/json, text/plain, */*','Accept-Encoding':'gzip, deflate, br','Accept-Language':'en-US,en;q=0.5',
         'Connection':'keep-alive','Content-Length':'127','Content-Type':'application/x-www-form-urlencoded','Host':'shoonya.finvasia.com','Origin':'https://shoonya.finvasia.com',
         'Referer':'https://shoonya.finvasia.com/','User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0'}
@@ -18,7 +18,7 @@ def login(response,email, password,pan):
     enctoken=s.post("https://shoonya.finvasia.com/jwt/token",headers=headers,data=data).text
     h=headers
     h.update({'Authorisation':'Token '+enctoken})
-    response=s.post("https://shoonya.finvasia.com/trade/login",headers=h,data=data)
+    return s.post("https://shoonya.finvasia.com/trade/login",headers=h,data=data)
 
 def fund(tokenid,key):
     temp={"token_id":tokenid,"keyid":key,"userid":"FA27632","clienttype":"C","usercode":"13549","pan_no":"CJEPG1375B"}
