e316b19b88345969ecde73ab1c94e08904257777
Rahulghuge94 <1994ghuge@gmail.com>
2020-12-10 01:01:37 +0530

    Update shoonya.py

--- trade/shoonya.py
+++ trade/shoonya.py
@@ -56,3 +56,25 @@ def orderbook():
     "Connection": "keep-alive",
     "Cookie": cookie, "Authorisation":"Token "+enctoken} 
     return s.post("https://shoonya.finvasia.com/trade/getOrderbook",headers=headers,data=data).json()
+
+def position():
+    temp={"row_1":"","row_2":"","exch":"","seg":"","product":"","v_mode":"","status":"","Inst":"","symbol":"","str_price":"","place_by":"","opt_type":"","exp_dt":"","token_id":tokenid,"keyid":key,"userid":"FA27632","clienttype":"C","usercode":"13549","pan_no":"CJEPG1375B"}
+    data={str(temp):""}
+    headers={"Host": "shoonya.finvasia.com",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0",
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Content-Length": "560",
+    "Referer": "https://shoonya.finvasia.com/",
+    "Origin": "https://shoonya.finvasia.com",
+    "Connection": "keep-alive",
+    "Cookie": cookie, "Authorisation":"Token "+enctoken} 
+    return s.post("https://shoonya.finvasia.com/trade/getNetposition",headers=headers,data=data).json()
+def order(price,qty,secid):
+    temp={"qty":qty,"price":price,"odr_type":"LMT","product_typ":"I","trg_prc":0,"validity":"DAY","disc_qty":0,"amo":false,"sec_id":"17963","inst_type":"EQUITY","exch":"NSE","buysell":"B","gtdDate":"0000-00-00","mktProtectionFlag":"N","mktProtectionVal":0,"settler":"000000000000","token_id":tokenid,"keyid":key,"userid":"FA27632","clienttype":"C","usercode":"13549","pan_no":"CJEPG1375B"}
+    data={str(temp):""}
+    headers={"Accept": "application/json text/plain */*","Accept-Encoding": "gzip deflate br","Accept-Language": "en-USen;q=0.5","Authorisation": "Token"+ enctoken,"Host": "shoonya.finvasia.com","Origin": "https://shoonya.finvasia.com","Referer": "https://shoonya.finvasia.com/",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0"}
+    return s.post("https://shoonya.finvasia.com/trade/placeorder",headers=headers,data=data).json()
