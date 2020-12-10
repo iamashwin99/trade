dc6ee736b4691fc91f3fd022803b11422aa383b1
Rahulghuge94 <1994ghuge@gmail.com>
2020-12-10 14:46:59 +0530

    Update shoonya.py

--- trade/shoonya.py
+++ trade/shoonya.py
@@ -87,12 +87,18 @@ def order(price,qty,secid):
     "Connection": "keep-alive",
     "Cookie": cookie, "Authorisation":"Token "+enctoken}
     return s.post("https://shoonya.finvasia.com/trade/placeorder",headers=headers,data=data).json()
-def getdata(secid=3045):
-    headers={"Accept":"*/*","Accept": "*/*","Accept-Encoding": "utf-8","Accept-Language": "en-USen;q=0.5","Connection":"keep-alive","Content-Length": "197","Content-Type": "application/json","Host": "shoonyabrd.finvasia.com",
-	"Origin": "https://shoonyabrd.finvasia.com","Referer": "https://shoonyabrd.finvasia.com/Charts/chartw.html","TE": "Trailers","User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0"}
-    temp={"SecIdxCode":secid,"Exch":1,"ScripIdLst":[],"Seg":1}
-    data={"Count":1,"Data":str(temp),"DoCompress":False,"RequestCode":129,"Reserved":"SMC0121","Source":"W","UserId":"FA27632"}
-    return s.post("https://shoonyabrd.finvasia.com/TickPub/api/Tick/LiveFeed?ciqrandom="+timestamp(),headers=headers,data=data).json()
+def getdata(secid):
+    temp={"Exch":"NSE","Seg":"E","ScripId":"3499","FromDate":1,"ToDate":2,"Time":1}
+    data={
+	"Count": 10,
+	"Data": str(temp),
+	"DoCompress": False,
+	"RequestCode": 800,
+	"Reserved": "",
+	"Source": "W",
+	"UserId": "FA27632"}
+    t=timestamp()
+    return s.post("https://shoonyabrd.finvasia.com/TickPub/api/Tick/LiveFeed?ciqrandom="+t,headers=headers,data=data).json()
 def timestamp():
     tmstmp=str(time.time())
     ciqrand=tmstmp[0]+tmstmp[1]+tmstmp[2]+tmstmp[3]+tmstmp[4]+tmstmp[5]+tmstmp[6]+tmstmp[7]+tmstmp[8]+tmstmp[9]+tmstmp[11]+tmstmp[12]+tmstmp[13]
