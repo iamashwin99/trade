e663687ca3271b768152da1e4730faf79f96d804
Rahulghuge94 <1994ghuge@gmail.com>
2020-12-14 22:08:00 +0530

    Update shoonya.py

--- trade/shoonya.py
+++ trade/shoonya.py
@@ -41,6 +41,7 @@ def fund():
     "Connection": "keep-alive",
     "Cookie": cookie, "Authorisation":"Token "+enctoken} 
     return s.post("https://shoonya.finvasia.com/trade/getLimits",headers=headers,data=data).json()
+
 def orderbook():
     temp={"row_1":"","row_2":"","exch":"","seg":"","product":"","status":"","inst":"","symbol":"","str_price":"","place_by":"","opt_type":"","exp_dt":"","token_id":tokenid,"keyid":key,"userid":"FA27632","clienttype":"C","usercode":"13549","pan_no":"CJEPG1375B"}
     data={str(temp):""}
@@ -72,6 +73,7 @@ def position():
     "Connection": "keep-alive",
     "Cookie": cookie, "Authorisation":"Token "+enctoken} 
     return s.post("https://shoonya.finvasia.com/trade/getNetposition",headers=headers,data=data).json()
+
 def order(price,qty,secid):
     temp={"qty":qty,"price":price,"odr_type":"LMT","product_typ":"I","trg_prc":0,"validity":"DAY","disc_qty":0,"amo":False,"sec_id":"17963","inst_type":"EQUITY","exch":"NSE","buysell":"B","gtdDate":"0000-00-00","mktProtectionFlag":"N","mktProtectionVal":0,"settler":"000000000000","token_id":tokenid,"keyid":key,"userid":"FA27632","clienttype":"C","usercode":"13549","pan_no":"CJEPG1375B"}
     data={str(temp):""}
@@ -87,25 +89,26 @@ def order(price,qty,secid):
     "Connection": "keep-alive",
     "Cookie": cookie, "Authorisation":"Token "+enctoken}
     return s.post("https://shoonya.finvasia.com/trade/placeorder",headers=headers,data=data).json()
+
 def getdata(secid):
-    headers={'Host': 'shoonyabrd.finvasia.com',
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0",
+    headers={'Host': 'shoonyabrd.finvasia.com',"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0",
     "Accept": "*/*","Accept-Encoding": "utf-8","Accept-Language": "en-USen;q=0.5",
     "Content-Length": "197",
     'Origin': 'https://shoonyabrd.finvasia.com',
     'Connection': 'keep-alive',
     'Referer': 'https://shoonyabrd.finvasia.com/Charts/chartw.html'}
-    temp={"Exch":"NSE","Seg":"E","ScripId":"3499","FromDate":1,"ToDate":2,"Time":1}
+    temp={"Exch":"NSE","Seg":"E","ScripId":secid,"FromDate":1,"ToDate":2,"Time":1}
     data={
-	"Count": 10,
-	"Data": str(temp),
-	"DoCompress": False,
-	"RequestCode": 800,
-	"Reserved": "",
-	"Source": "W",
-	"UserId": "FA27632"}
+    "Count": 10,
+    "Data": str(temp),
+    "DoCompress": False,
+    "RequestCode": 800,
+    "Reserved": "",
+    "Source": "W",
+    "UserId": "FA27632"}
     t=timestamp()
-    return s.post("https://shoonyabrd.finvasia.com/TickPub/api/Tick/LiveFeed?ciqrandom="+t,headers=headers,data=data).json()
+    return pd.DataFrame(s.post("https://shoonyabrd.finvasia.com/TickPub/api/Tick/LiveFeed?ciqrandom="+t,headers=headers,data=data).json())
+
 def timestamp():
     tmstmp=str(time.time())
     ciqrand=tmstmp[0]+tmstmp[1]+tmstmp[2]+tmstmp[3]+tmstmp[4]+tmstmp[5]+tmstmp[6]+tmstmp[7]+tmstmp[8]+tmstmp[9]+tmstmp[11]+tmstmp[12]+tmstmp[13]
