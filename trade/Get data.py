3e79f103aa326450b21e921f28ae591b49ee8138
Rahulghuge94 <1994ghuge@gmail.com>
2020-12-15 19:30:23 +0530

    Update Get data.py

--- trade/Get data.py
+++ trade/Get data.py
@@ -1,15 +1,10 @@
+#Author:-Rahul Ghuge
 
-from api5paisa import Api5Paisa
 from time import sleep
 import pandas as pd
 
-api = Api5Paisa()
-api.login("8055426572", "Kapil@123","22111994")
 
-scrcode=['15083',]
 
-def getdata(Etyp,scrip):
-    return api.post('/Trade/Chart/FetchQuoteData',{"Exch": "N","ExchType": Etyp,"LastRequestTime": "1TODAY","ScripCode": scrip})
 
 def ma(period,pricedata):
     return pricedata["Close"].rolling(window=period).mean()
@@ -22,8 +17,6 @@ def vwap(pricedf):
     pricedf['vwap']=(((pricedf['Close']+pricedf['Low']+pricedf['High'])/3)*pricedf['Volume']).cumsum())/pricedf['Volume'].cumsum()
     return pricedf['vwap'].iloc[-1]
 
-def vwap_signal(dfc-2,dfv-2,dfc-1,dfv-1):
-    if 
-    
+
     
     
