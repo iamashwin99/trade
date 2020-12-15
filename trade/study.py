e2d81cf6ee387cb4de53303ab53578f45077daaa
Rahulghuge94 <1994ghuge@gmail.com>
2020-12-15 19:37:10 +0530

    Update and rename Get data.py to study.py

--- trade/study.py
+++ trade/study.py
@@ -1,22 +1,26 @@
 #Author:-Rahul Ghuge
 
-from time import sleep
-import pandas as pd
-
-
-
-
 def ma(period,pricedata):
+    """
+    Arg=
+    period=moving average period
+    pricedata= intraday data of today in dataframe
+    """
     return pricedata["Close"].rolling(window=period).mean()
 
 def ma_last(period,pricedata):
+    """
+    Arg=
+    period=moving average period
+    pricedata= intraday data of today in dataframe
+    """
     temp=pricedata["Close"].rolling(window=period).mean()
     return temp.iloc[-1]
 
 def vwap(pricedf):
+    """
+    Arg:-
+    pricedata= intraday data of today in dataframe
+    """
     pricedf['vwap']=(((pricedf['Close']+pricedf['Low']+pricedf['High'])/3)*pricedf['Volume']).cumsum())/pricedf['Volume'].cumsum()
     return pricedf['vwap'].iloc[-1]
-
-
-    
-    
