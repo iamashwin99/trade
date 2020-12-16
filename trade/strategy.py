891b1ff428c3bb0da42d66197cd79fdcd437fa5e
Rahulghuge94 <1994ghuge@gmail.com>
2020-12-16 23:12:10 +0530

    Update strategy.py

--- trade/strategy.py
+++ trade/strategy.py
@@ -2,9 +2,10 @@ from trade.study import *
 
 
 def trade_on_vwap(dfdata):
-    vwap_value=vwap_last(dfdta)
+    vwap_value=vwap_last(dfdata)
     if vwap_value>dfdata["Close"].iloc[-1] and vwap_value>dfdata["Open"].iloc[-1]:
        return "B"
-    
     if vwap_value<dfdata["Close"].iloc[-1] and vwap_value<dfdata["Open"].iloc[-1]:
        return "S"
+    else:
+        print("No Signal Generated")
